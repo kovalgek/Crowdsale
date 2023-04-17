e47505f6d1b87a311ccc5a849bca78e9219c6dc9
Anton Kovalchuk <koval400k@gmail.com>
2023-04-18 00:24:01 +0200

    add tests to simple coin contract

--- contracts/Coin/SimpleCoin.sol
+++ contracts/Coin/SimpleCoin.sol
@@ -4,27 +4,42 @@ pragma solidity ^0.8.17;
 import "./../Ownable.sol";
 
 contract SimpleCoin is Ownable {
-    mapping(address => uint256) coinBalance;
-    mapping(address => mapping(address => uint256)) allowance;
-    mapping(address => bool) frozenAccount;
+    mapping(address => uint256) private coinBalance;
+    mapping(address => mapping(address => uint256)) private allowance;
 
     event Transfer(address indexed _from, address indexed _to, uint256 _value);
-    event FrozenAccount(address _target, bool _frozen);
 
+    /**
+    * @notice Constructs contract
+    * @param _initialSupply initial coins amount.
+    */
     constructor(uint256 _initialSupply) {
         mint(owner, _initialSupply);
     }
 
+    /**
+    * @notice Mints coins
+    * @param _recipient an account that receives coins,
+    * @param _mintedAmount: an amounted of received coins.
+    */
     function mint(address _recipient, uint256 _mintedAmount) public onlyOwner {
         coinBalance[_recipient] += _mintedAmount;
         emit Transfer(owner, _recipient, _mintedAmount);
     }
 
-    function freezeAccount(address _target, bool _freeze) public onlyOwner {
-        frozenAccount[_target] = _freeze;
-        emit FrozenAccount(_target, _freeze);
+    /**
+    * @notice Returns balance of `_account`
+    * @param _account account that ballance is requested.
+    */
+    function balanceOf(address _account) public view returns (uint256) {
+        return coinBalance[_account];
     }
 
+    /**
+    * @notice Transfers coins from sender to another account
+    * @param _to an account that receives coins,
+    * @param _amount: an amount of transfered coins.
+    */
     function transfer(address _to, uint256 _amount) virtual public {
         require(coinBalance[msg.sender] > _amount);
         require(coinBalance[_to] + _amount >= coinBalance[_to]);
@@ -35,19 +50,30 @@ contract SimpleCoin is Ownable {
         emit Transfer(msg.sender, _to, _amount);
     }
 
-    function authorize(address _authorizedAccount, uint256 _allowance) public {
-        allowance[msg.sender][_authorizedAccount] = _allowance;
-    }
-
+    /**
+    * @notice Transfers coins from `_from` to `_to` account.
+    * @param _from an account from whome coins are taken,
+    * @param _to an account that receives coins,
+    * @param _amount an amount of transfered coins.
+    */
     function transferFrom(address _from, address _to, uint256 _amount) virtual public {
         require(coinBalance[_from] > _amount);
         require(coinBalance[_to] + _amount >= coinBalance[_to]);
-        require(_amount <= allowance[_from][msg.sender]);
+        // require(_amount <= allowance[_from][msg.sender]);
 
         coinBalance[_from] -= _amount;
         coinBalance[_to] += _amount;
-        allowance[_from][msg.sender] -= _amount;
+        // allowance[_from][msg.sender] -= _amount;
 
         emit Transfer(_from, _to, _amount);
+    }
+
+    /**
+    * @notice Authorizes account for managing its coins.
+    * @param _authorizedAccount an account that is going to be authorized,
+    * @param _allowance amount of coints that will be allowed to manage for authorized account.
+    */
+    function authorize(address _authorizedAccount, uint256 _allowance) public {
+        allowance[msg.sender][_authorizedAccount] = _allowance;
     }
 }
---
+++ test/SimpleCoin.js
@@ -0,0 +1,57 @@
+const { expect } = require("chai");
+const hre = require("hardhat");
+const { loadFixture } = require("@nomicfoundation/hardhat-network-helpers");
+
+describe("SimpleCoint", function() {
+
+    async function deploySimpleCoinFixture() {
+
+        // Contracts are deployed using the first signer/account by default
+        const [owner, account1, account2] = await ethers.getSigners();
+    
+        const TEN_COINS = 10;
+        const initialSupply = TEN_COINS;
+
+        const SimpleCoin = await ethers.getContractFactory("SimpleCoin");
+        const simpleCoin = await SimpleCoin.deploy(initialSupply);
+    
+        return { simpleCoin, initialSupply, owner, account1, account2 };
+      }
+
+    it("Should init owner with coins", async function() {
+        const { simpleCoin, owner} = await loadFixture(deploySimpleCoinFixture);
+        expect(await simpleCoin.balanceOf(owner.address)).to.equal(10);
+    });
+
+    it("Should mint coins", async function() {
+        const { simpleCoin, initialSupply, owner, account1 } = await loadFixture(deploySimpleCoinFixture);
+        
+        await simpleCoin.mint(account1.address, 65);
+        expect(await simpleCoin.balanceOf(account1.address)).to.equal(65);
+
+        await simpleCoin.mint(account1.address, 5);
+        expect(await simpleCoin.balanceOf(account1.address)).to.equal(70);
+    });
+
+    it("Should transfer coins from owner to another account", async function() {
+        const { simpleCoin, initialSupply, owner, account1 } = await loadFixture(deploySimpleCoinFixture);
+
+        await simpleCoin.mint(account1.address, 6);
+        await simpleCoin.transfer(account1.address, 2);
+
+        expect(await simpleCoin.balanceOf(owner.address)).to.equal(8);
+        expect(await simpleCoin.balanceOf(account1.address)).to.equal(8);
+    });
+
+    it("Should transfer coins from account1 to account2", async function() {
+        const { simpleCoin, initialSupply, owner, account1, account2 } = await loadFixture(deploySimpleCoinFixture);
+
+        await simpleCoin.mint(account1.address, 6);
+        await simpleCoin.mint(account2.address, 7);
+
+        await simpleCoin.transferFrom(account1.address, account2.address, 3);
+
+        expect(await simpleCoin.balanceOf(account1.address)).to.equal(3);
+        expect(await simpleCoin.balanceOf(account2.address)).to.equal(10);
+    });
+});
